a4c4a972e51b48b423b8e2b692f7aaa0b4cdc163
Robert Yale Home <rcy6@duke.edu>
2020-05-27 22:43:20 -0400

    Faster method of data retrieval

--- schema.sql
+++ schema.sql
@@ -29,3 +29,21 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.prices
     OWNER to postgres;
+
+CREATE TABLE public.volume
+(
+    id bigint NOT NULL DEFAULT nextval('volume_id_seq'::regclass),
+    item_id integer NOT NULL,
+    day bigint NOT NULL,
+    volume bigint NOT NULL,
+    CONSTRAINT volume_pkey PRIMARY KEY (id),
+    CONSTRAINT item_id_to_id FOREIGN KEY (item_id)
+        REFERENCES public.items (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.volume
+    OWNER to postgres;
